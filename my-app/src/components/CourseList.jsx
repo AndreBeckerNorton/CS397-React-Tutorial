2093b91bcf9d294724b9dc07ccae1f15a19273d5
Andre Becker Norton <andrenorton2022@u.northwestern.edu>
2022-10-03 15:29:50 -0500

    Update CourseList.jsx

--- my-app/src/components/CourseList.jsx
+++ my-app/src/components/CourseList.jsx
@@ -1,12 +1,11 @@
-import CourseListIndiv from "./CourseListIndiv";
-
-const CourseList = ({schedule}) => {
-    // <div> <h1>{schedule.title}</h1></div>
-    <div class='courseList'>
-        { Object.entries(schedule.courses).map(([id, course]) => 
-        <li> {course.term} CS {course.number}: {course.title} </li>) }
-
-    </div>
+const CourseList = ({ courses }) => {
+    return (
+      <div>
+        {Object.entries(courses).map(([id, course]) => (
+          <p key={id}>{`${course.term} CS ${course.number}: ${course.title}`}</p>
+        ))}
+      </div>
+    );
 };
-
-  export default CourseList; 
+  
+  export default CourseList;
