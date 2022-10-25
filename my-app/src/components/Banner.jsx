202e007708bb1a0d7c236cb1a82220ce44686279
Andre Becker Norton <andrenorton2022@u.northwestern.edu>
2022-10-25 00:54:11 -0500

    add authentication

--- my-app/src/components/Banner.jsx
+++ my-app/src/components/Banner.jsx
@@ -1,7 +1,27 @@
-const Banner = ({title}) => {
-    return (
-    <h1>{title}</h1>
-    );
+import { signInWithGoogle, signOut, useAuthState } from '../utilities/firebase';
+
+const Banner = ({ title }) => {
+  return (
+    <nav>
+      <h1>{title}</h1>
+      <AuthButton />
+    </nav>
+  );
 };
-  
+
+const SignInButton = () => (
+  <button onClick={signInWithGoogle}>Sign in</button>
+);
+
+const SignOutButton = () => (
+  <button onClick={signOut}>Sign out</button>
+);
+
+const AuthButton = () => {
+  const [user] = useAuthState();
+  return user ? <SignOutButton /> : <SignInButton />;
+};
+
+const activation = ({ isActive }) => isActive ? 'active' : 'inactive';
+
 export default Banner;
